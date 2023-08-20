5898cc1da652e1132516ab4c36e1c78f8ec52e53
samuelikoli <ayibanimiikoli@gmail.com>
2023-08-20 18:16:38 +0100

    cron update on vercel

--- api/cron.js
+++ api/cron.js
@@ -11,6 +11,7 @@ export default async function handler() {
     Mail('ayibanimiikoli@gmail.com', 'www.google.com')
     // const number = Math.floor(Math.random() * 9999);
     // console.log('Number is', number);
+    console.log('Number is fine');
     // const newNum = new Number({ number });
     // await newNum.save();
     return
