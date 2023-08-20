c5a0ddabdca4742c6850706794a1b57eb79eadcf
samuelikoli <ayibanimiikoli@gmail.com>
2023-08-20 14:34:18 +0100

    cron update on vercel

--- api/cron.js
+++ api/cron.js
@@ -1,7 +1,7 @@
 const Number = require('../models/numbers');
 
 
-export default async function () {
+export default async function handler() {
     const number = Math.floor(Math.random() * 9999);
     console.log('Number is', number);
     const newNum = new Number({ number });
